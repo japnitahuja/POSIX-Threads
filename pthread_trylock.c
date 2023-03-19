dc76b997421cfaf37d80e28bab7fea6d0d82712e
japnitahuja <japnit.ahuja@gmail.com>
2023-03-19 10:25:35 -0400

    new program of try lock

--- pthread_trylock.c
+++ pthread_trylock.c
@@ -1,9 +1,17 @@
 /*
-This program illustrates how to use mutex to avoid race conditions.
+This program illustrates how to use try lock.
 
 Exepected Output:
-The total should be 2000000
-The total sum is 2000000
+Thread 8 did not get the lock!
+Thread 7 did not get the lock!
+Thread 2 did not get the lock!
+Thread 0 did not get the lock!
+Thread 9 did not get the lock!
+Thread 3 did not get the lock!
+Thread 6 did not get the lock!
+Thread 5 did not get the lock!
+Thread 4 got the lock!
+Thread 1 did not get the lock!
 
 Author: japnitahuja
 Date: 19th March 2023
@@ -13,42 +21,62 @@ Date: 19th March 2023
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h> //library for pthreads
+#include <time.h>
+#define NUM_THREADS 10
 
-int sum = 0;
-pthread_mutex_t mutex;
+pthread_mutex_t mutex; // creation of mutex
 
-// increases value of variable sum
-void *increase_sum()
+void *try_lock(void *arg)
 {
-    for (int i = 0; i < 1000000; i++)
+    int threadID = *(int *)arg;
+
+    // making thread sleep for random time between
+    //  0 to 3
+    srand(time(NULL));
+    int sleeptime = rand() % 3;
+    sleep(sleeptime);
+
+    // if thred gets the lock
+    if (pthread_mutex_trylock(&mutex) == 0)
     {
-        pthread_mutex_lock(&mutex);
-        sum++;
+        printf("Thread %d got the lock!\n", threadID);
+        sleep(1);
         pthread_mutex_unlock(&mutex);
     }
+    // if thread does not get the lock
+    else
+    {
+        printf("Thread %d did not get the lock!\n", threadID);
+    }
+
+    free(arg)
 }
 
 int main(int argc, char *argv[])
 {
     // variable to store the threads
-    pthread_t t1, t2;
+    pthread_t threads[NUM_THREADS];
 
     // initialisation of mutex
     pthread_mutex_init(&mutex, NULL);
 
     // thread creation
-    pthread_create(&t1, NULL, &increase_sum, NULL);
-    pthread_create(&t2, NULL, &increase_sum, NULL);
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        int *x = malloc(sizeof(int));
+        *x = i; // sending arg as thread ID
+        // fourth argument is the void pointer argument
+        pthread_create(&threads[i], NULL, &try_lock, x);
+    }
 
-    // wait for the child thread to join main thread
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
+    // thread joining
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
 
     // destroying mutex
     pthread_mutex_destroy(&mutex);
 
-    // print sum after both threads join
-    printf("The total should be 2000000\n");
-    printf("The total sum is %d \n", sum);
     return 0;
 }
